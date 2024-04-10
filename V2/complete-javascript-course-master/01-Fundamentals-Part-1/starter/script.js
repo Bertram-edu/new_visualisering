9aa10af8e1e386c67c21b0d342de76a0f6cd520e
Bertram-edu <63588@edu.campusvejle.dk>
2024-04-10 13:06:32 +0200

    work in progress

--- V2/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ V2/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -265,4 +265,90 @@ if (hasDriversLicense && hasGoodVision && !isTried) {
     console.log("Someone else should drive...")
 }
 */
+/*
+// my version
+const dolphinsScore = [96, 108, 89]
+const koalasScore = [88, 91, 110]
+
+let dolphinFinalNumber = 0
+for (let i = 0; i < dolphinsScore.length; i++) {
+    dolphinFinalNumber += dolphinsScore[i]
+}
+const scoreDolphins = dolphinFinalNumber / dolphinsScore.length
+
+let koalaFinalNumber = 0
+for (let i = 0; i < koalasScore.length; i++) {
+    koalaFinalNumber += koalasScore[i]
+}
+const scoreKoalas = koalaFinalNumber / koalasScore.length
+
+if (scoreDolphins > scoreKoalas) {
+    console.log("Dolphins win the trophy")
+} else if (scoreDolphins < scoreKoalas) {
+    console.log("Koalas win the trophy")
+} else if (scoreDolphins === scoreKoalas) {
+    console.log("Both win the trophy")
+}
+*/
+/*
+// his version
+const scoreDolphins = (97 + 112 + 80) / 3;
+const scoreKoalas = (109 + 95 + 50) / 3;
+console.log(scoreDolphins, scoreKoalas)
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log("dolphins win the trophy");
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+    console.log("koalas win the trophy");
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log("both win the trophy")
+} else {
+    console.log("No one winds the trophy D:")
+}
+*/
+
+/*
+const day = "thursday";
+
+switch (day) {
+    case "monday": // day === "monday"
+        console.log("Plan course structure");
+        console.log("Go to coding meetup");
+        break;
+    case "tuesday":
+        console.log("prepare theory videos");
+        break
+    case "wednesday":
+    case "thursday":
+        console.log("Write code examples");
+        break
+    case "friday":
+        console.log("Record videos");
+        break
+    case "saturday":
+    case "sunday":
+        console.log("Enjot the weekend :D");
+        break
+    default:
+        console.log("Not a valid day!")
+}
+
+
+// const day = prompt("what day?")
+
+if (day === "monday") {
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+    console.log("prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+    console.log("Write code examples");
+} else if (day === "friday") {
+    console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+    console.log("Enjot the weekend :D");
+} else {
+    console.log("Not a valid day!")
+}
+*/
 
